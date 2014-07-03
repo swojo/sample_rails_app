5a4d2f0c155197f15912df003e429e12da78c67e
Samantha <wojtowi4@illinois.email>
2014-07-03 16:13:43 -0500

    Finish static pages

---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,40 @@
+require 'spec_helper'
+
+describe "StaticPages" do
+  describe 'Home Page' do
+    it 'should have the content "Sample App" ' do
+      visit '/static_pages/home' 
+      expect(page).to have_content('Sample App')
+    end
+
+    it 'should have the right title' do
+      visit '/static_pages/home'
+      expect(page).to have_title("Home")
+    end
+  end
+
+  describe 'Help Page' do
+    it 'should have the content "Help" ' do
+      visit '/static_pages/help' 
+      expect(page).to have_content('Help')
+    end
+
+    it 'should have the right title' do
+      visit '/static_pages/help'
+      expect(page).to have_title("Help")
+    end
+  end
+
+  describe 'About Page' do
+    it 'should have the content "About" ' do
+      visit '/static_pages/about' 
+      expect(page).to have_content('About')
+    end
+
+    it 'should have the right title' do
+      visit '/static_pages/about'
+      expect(page).to have_title("About Us")
+    end
+  end
+    
+end
